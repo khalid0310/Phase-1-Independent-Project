37cf2f45987027f66dca92722316d35d98b55350
khalid <khalid.mohamed@moringaschool.com>
2023-10-13 01:16:07 +0300

    initial setup

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const pokemonList = document.getElementById('pokemon-info');
+    const searchButton = document.getElementById('search-button');
+    const searchInput = document.getElementById('search-input');
+});
